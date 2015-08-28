eda6a1ee1408a800c6152c47e29abf62400592c8
Dries Hoet <drhoet@users.noreply.github.com>
2015-08-28 19:57:58 +0200

    Added first DatabaseManager

---
+++ DatabaseManager/MainForm.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace DatabaseManager
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.initializeNewDatabaseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openDatabaseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importDictionaryToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.dsData = new System.Data.DataSet();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dsData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.dataToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(832, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.initializeNewDatabaseToolStripMenuItem,
+            this.openDatabaseToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.fileToolStripMenuItem.Text = "Database";
+            // 
+            // initializeNewDatabaseToolStripMenuItem
+            // 
+            this.initializeNewDatabaseToolStripMenuItem.Name = "initializeNewDatabaseToolStripMenuItem";
+            this.initializeNewDatabaseToolStripMenuItem.Size = new System.Drawing.Size(201, 22);
+            this.initializeNewDatabaseToolStripMenuItem.Text = "Initialize new database...";
+            this.initializeNewDatabaseToolStripMenuItem.Click += new System.EventHandler(this.initializeNewDatabaseToolStripMenuItem_Click);
+            // 
+            // openDatabaseToolStripMenuItem
+            // 
+            this.openDatabaseToolStripMenuItem.Name = "openDatabaseToolStripMenuItem";
+            this.openDatabaseToolStripMenuItem.Size = new System.Drawing.Size(201, 22);
+            this.openDatabaseToolStripMenuItem.Text = "Open database...";
+            this.openDatabaseToolStripMenuItem.Click += new System.EventHandler(this.openDatabaseToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(201, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // dataToolStripMenuItem
+            // 
+            this.dataToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.importDictionaryToolStripMenuItem1});
+            this.dataToolStripMenuItem.Name = "dataToolStripMenuItem";
+            this.dataToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.dataToolStripMenuItem.Text = "Data";
+            // 
+            // importDictionaryToolStripMenuItem1
+            // 
+            this.importDictionaryToolStripMenuItem1.Name = "importDictionaryToolStripMenuItem1";
+            this.importDictionaryToolStripMenuItem1.Size = new System.Drawing.Size(175, 22);
+            this.importDictionaryToolStripMenuItem1.Text = "Import dictionary...";
+            // 
+            // dgvData
+            // 
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvData.Location = new System.Drawing.Point(0, 24);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.Size = new System.Drawing.Size(832, 503);
+            this.dgvData.TabIndex = 1;
+            this.dgvData.VirtualMode = true;
+            this.dgvData.CellValueNeeded += new System.Windows.Forms.DataGridViewCellValueEventHandler(this.dgvData_CellValueNeeded);
+            // 
+            // dsData
+            // 
+            this.dsData.DataSetName = "NewDataSet";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 527);
+            this.Controls.Add(this.dgvData);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Language Tools Database Manager";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dsData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem initializeNewDatabaseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openDatabaseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dataToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importDictionaryToolStripMenuItem1;
+        private System.Windows.Forms.DataGridView dgvData;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Data.DataSet dsData;
+    }
+}
+
---
+++ DatabaseManager/MainForm.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DatabaseManager
+{
+    public partial class MainForm : Form
+    {
+        private SQLiteConnection conn = null;
+        private SQLiteDataAdapter dataAdapter = null;
+        private SQLiteCommandBuilder commandBuilder = null;
+        private DataTable table;
+        public MainForm()
+        {
+            InitializeComponent();
+            SQLiteConnectionStringBuilder connBuilder = new SQLiteConnectionStringBuilder();
+            bool newDb = !File.Exists("default.db");
+            connBuilder.DataSource = "default.db";
+            connBuilder.Version = 3;
+            loadDatabase(connBuilder.ToString());
+            if(newDb)
+            {
+                initializeDatabase();
+            }
+            loadTable("lemma");
+        }
+
+        private void loadDatabase(string connectionString)
+        {
+            closeDatabase();
+            conn = new SQLiteConnection(connectionString);
+            conn.Open();
+        }
+
+        private void closeDatabase()
+        {
+            if(conn != null)
+            {
+                commandBuilder = new SQLiteCommandBuilder(dataAdapter);
+                DataTable changes = table.GetChanges();
+                if (changes != null)
+                {
+                    dataAdapter.Update(changes);
+                }
+                conn.Close();
+            }
+        }
+
+        private void loadTable(string tableName)
+        {
+            /*DataProvider provider = new DataProvider(conn, tableName);
+            memoryCache = new Cache(provider, 100);
+            dgvData.Columns.Clear();
+            foreach(DataColumn column in provider.Columns)
+            {
+                dgvData.Columns.Add(column.ColumnName, column.ColumnName);
+            }
+            dgvData.RowCount = provider.RowCount;*/
+            dataAdapter = new SQLiteDataAdapter("select * from lemma", conn);
+            table = new DataTable("lemma");
+            dataAdapter.Fill(table);
+            dgvData.DataSource = table;
+            dgvData.Columns["id"].Visible = false;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void initializeNewDatabaseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                SQLiteConnectionStringBuilder connBuilder = new SQLiteConnectionStringBuilder();
+                connBuilder.DataSource = saveFileDialog.FileName;
+                connBuilder.Version = 3;
+                loadDatabase(connBuilder.ToString());
+                initializeDatabase();
+                loadTable("lemma");
+            }
+        }
+
+        private void initializeDatabase()
+        {
+            SQLiteCommand command = new SQLiteCommand("create table lemma (id integer primary key not null, text varchar(100), gender int)", conn);
+            command.ExecuteNonQuery();
+        }
+
+        private void openDatabaseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                SQLiteConnectionStringBuilder connBuilder = new SQLiteConnectionStringBuilder();
+                connBuilder.DataSource = openFileDialog.FileName;
+                connBuilder.Version = 3;
+                loadDatabase(connBuilder.ToString());
+                loadTable("lemma");
+            }
+        }
+
+        private void dgvData_CellValueNeeded(object sender, DataGridViewCellValueEventArgs e)
+        {
+            //e.Value = memoryCache.RetrieveElement(e.RowIndex, e.ColumnIndex);
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            closeDatabase();
+        }
+    }
+}
